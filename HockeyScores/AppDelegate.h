1327cd8f7841f87edab4b772f65e32c026014571
Jason Leyrer <jleyrer@gmail.com>
2013-11-12 19:22:05 -0600

    Initial Commit

---
+++ HockeyScores/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HockeyScores
+//
+//  Created by Jason Leyrer on 11/12/13.
+//  Copyright (c) 2013 Jason Leyrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HockeyScores/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  HockeyScores
+//
+//  Created by Jason Leyrer on 11/12/13.
+//  Copyright (c) 2013 Jason Leyrer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
