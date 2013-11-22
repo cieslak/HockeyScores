ea8350e0ecf2424e302d8b3f74e1f9851f55411f
Chris Cieslak <chris@electropuf.com>
2013-11-21 18:56:03 -0600

    Show more efficient way to alloc date formatters

--- HockeyScores/Game.m
+++ HockeyScores/Game.m
@@ -26,21 +26,38 @@
             self.homeTeamCity = [gameData objectForKey:@"homeTeamCity"];
             self.homeTeamName = [gameData objectForKey:@"homeTeamName"];
             self.isPlaying = (BOOL)[gameData objectForKey:@"isPlaying"];
+            
+            /*  NSDateFormatters are really expensive to alloc and init. Since you might have a lot of Game objects,
+             allocing 2 formatters on init of each object can get inefficient. Instead, use 2 static variables.
+             These will persist across all instances of the class.
+             */
+            
+            static NSDateFormatter *sESTTimeFormat = nil, *sLocalTimeFormat = nil;
+            
+            /* OK, now run the alloc init inside a dispatch_once block. Dispatch_once is exactly what it sounds like-
+             the code inside the block is only run once in the life of the application. So upon the first call to
+             a Game object's initWithGameDataMethod: the static variables will be assigned. After that, each init
+             call will use the same 2 formatters we alloced inside this block. */
+            
+            static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                sESTTimeFormat = [[NSDateFormatter alloc] init];
+                [sESTTimeFormat setTimeZone:[NSTimeZone timeZoneWithName:@"EST"]];
+                [sESTTimeFormat setDateFormat:@"h:mm a"];
+                [sESTTimeFormat setLocale:[[NSLocale alloc]
+                                          initWithLocaleIdentifier:@"en_US_POSIX"]];
+                
+                sLocalTimeFormat = [[NSDateFormatter alloc] init];
+				[sLocalTimeFormat setTimeStyle:NSDateFormatterShortStyle];
+                [sLocalTimeFormat setLocale:[[NSLocale alloc]
+                                            initWithLocaleIdentifier:@"en_US_POSIX"]];
+            });
 			
-			NSDateFormatter *estTimeFormat = [[NSDateFormatter alloc] init];
-			[estTimeFormat setTimeZone:[NSTimeZone timeZoneWithName:@"EST"]];
-			[estTimeFormat setDateFormat:@"h:mm a"];
-            [estTimeFormat setLocale:[[NSLocale alloc]
-                                      initWithLocaleIdentifier:@"en_US_POSIX"]];
-			NSDate *estTime = [estTimeFormat
+			NSDate *estTime = [sESTTimeFormat
                                dateFromString:[gameData objectForKey:@"period"]];
             
 			if (estTime) {
-                NSDateFormatter *localTimeFormat = [[NSDateFormatter alloc] init];
-				[localTimeFormat setTimeStyle:NSDateFormatterShortStyle];
-                [localTimeFormat setLocale:[[NSLocale alloc]
-                                          initWithLocaleIdentifier:@"en_US_POSIX"]];
-				self.period = [localTimeFormat stringFromDate:estTime];
+				self.period = [sLocalTimeFormat stringFromDate:estTime];
 			} else {
 				self.period = [gameData objectForKey:@"period"];
 			}
